9cc2e8264096f2a7ed6b90dac51675e9f9d7bd2d
edu-mguerra <eduguerra.dev@gmail.com>
2025-03-08 09:29:23 -0300

    left

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,33 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './MovieRow.css'
 
 
-export default ({title, items}) => {
+export default ({ title, items }) => {
     const [scrollX, setScrollX] = useState(0)
 
     const handleLeftArrow = () => {
         let x = scrollX + Math.round(window.innerWidth / 2)
-        if(x > 0){//isso é apenas uma segurança
+        if (x > 0) {//isso é apenas uma segurança
             x = 0
         }
         setScrollX(x)
     }
-    const handleRightArrow = () =>{
+    const handleRightArrow = () => {
         let x = scrollX - Math.round(window.innerWidth / 2)
         let listW = items.results.length * 150;
-        if((window.innerWidth - listW) > x){
+        if ((window.innerWidth - listW) > x) {
             x = (window.innerWidth - listW) - 60
-        }       
-        setScrollX(x) 
+        }
+        setScrollX(x)
     }
-    return(
-        <div className="movieRow">  
-            <h2>{title}</h2> 
+    return (
+        <div className="movieRow">
+            <h2>{title}</h2>
             <div className="movieRow--left" onClick={handleLeftArrow}>
-                <p style={{fontSize: 50}}>{"<"}</p>
+                <p >{"<"}</p>
             </div>
             <div className="movieRow--right" onClick={handleRightArrow}>
-                <p style={{fontSize: 50}}>{">"}</p>
+                <p >{">"}</p>
             </div>
             <div className="movieRow--listarea">
 
@@ -38,14 +38,14 @@ export default ({title, items}) => {
                     {items.results.length > 0 && items.results.map((item, key) => (
 
                         <div key={key} className="movieRow--item">
-                          <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
 
                         </div>
                     ))}
-            </div>
                 </div>
+            </div>
 
-               
+
         </div>
     )
 }
